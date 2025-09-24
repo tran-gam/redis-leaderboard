0c7b3fd61e388fff3e8b01b5abb651bd8da18653
Chanh Tran <chanh.tran@redis.com>
2025-09-24 16:44:39 -0500

    added backend api

--- public/src/components/Board.jsx
+++ public/src/components/Board.jsx
@@ -44,24 +44,3 @@ function sort(data) {
     }
   });
 }
-
-// function between(data, between) {
-//   const today = new Date();
-//   const previous = new Date(today);
-//   previous.setDate(previous.getDate() - (between + 1));
-
-//   let filter = data.filter((val) => {
-//     let userDate = new Date(val.dt);
-//     if (between == 0) return val;
-//     return previous <= userDate && today >= userDate;
-//   });
-
-//   // sort with asending order
-//   return filter.sort((a, b) => {
-//     if (a.score === b.score) {
-//       return b.score - a.score;
-//     } else {
-//       return b.score - a.score;
-//     }
-//   });
-// }
